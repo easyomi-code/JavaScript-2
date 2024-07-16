4fac2be2382fcccafc3bf97dd2229b893e6efa24
easyomi-code <easyomi.code@gmail.com>
2024-07-16 21:19:59 +0900

    first commit

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,62 @@
+/*
+// 1. Linking a JavaScript File(JS 파일 연결)
+let js = 'amazing';
+console.log(1 + 5 + 8 + 16 + 28 + 33 + 45);
+
+// -----------------------------------------
+
+// 2-1. Values and Variables(값, 변수)
+console.log('Eunyomi');
+console.log(24);
+
+let firstName = 'Lee';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// -----------------------------------------
+
+// 2-2. Variable name conventions(변수 이름 규칙)
+let easyomi_code = 'EC';
+let $function = 26;
+
+let person = 'eunyomi';
+let PI = 3.141592;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Police Officer';
+
+let job1 = 'programmer';
+let job2 = 'police officer';
+
+console.log(myFirstJob);
+
+// -----------------------------------------
+
+// 3. Data Types(데이터 타입)
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log('javascriptIsFun: ', typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Eunyomi');
+
+javascriptIsFun = 'YES!';
+console.log('javascriptIsFun: ', typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2024;
+console.log(typeof year);
+
+console.log(typeof null);
+
+*/
+
+// -----------------------------------------
+
+// 4. let, const and var (변수 선언)
