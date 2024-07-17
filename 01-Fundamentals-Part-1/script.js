cb05fa7067d7452db19846fbd245b7d57aa711bf
easyomi-code <easyomi.code@gmail.com>
2024-07-17 21:54:44 +0900

    let, const and var

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,15 +1,15 @@
 /*
-// 1. Linking a JavaScript File(JS 파일 연결)
+// 1. Linking a JavaScript File (JS 파일 연결)
 let js = 'amazing';
 console.log(1 + 5 + 8 + 16 + 28 + 33 + 45);
 
 // -----------------------------------------
 
-// 2-1. Values and Variables(값, 변수)
+// 2-1. Values and Variables (값, 변수)
 console.log('Eunyomi');
 console.log(24);
 
-let firstName = 'Lee';
+let firstName = 'Eunyeong';
 
 console.log(firstName);
 console.log(firstName);
@@ -17,7 +17,7 @@ console.log(firstName);
 
 // -----------------------------------------
 
-// 2-2. Variable name conventions(변수 이름 규칙)
+// 2-2. Variable name conventions (변수 이름 규칙)
 let easyomi_code = 'EC';
 let $function = 26;
 
@@ -34,7 +34,7 @@ console.log(myFirstJob);
 
 // -----------------------------------------
 
-// 3. Data Types(데이터 타입)
+// 3. Data Types (데이터 타입)
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
@@ -55,8 +55,28 @@ console.log(typeof year);
 
 console.log(typeof null);
 
-*/
-
 // -----------------------------------------
 
 // 4. let, const and var (변수 선언)
+// let - 빈 변수 선언 or 값 변경이 가능한 변수
+let age = 30;
+age = 31;
+
+// const - 상수 변수에 대한 할당(변경할 수 없는 변수)
+const birthYear = 1999;
+// birthYear = 1990; (X)
+// const job; (X)
+
+// var - 옛날 방식(ES6 이전)
+var job = 'programmer';
+job = 'teacher';
+
+// 변수 선언X - (terrible)
+lastName = 'Lee';
+console.log(lastName);
+
+// ※ 항상 변수를 적절이 선언하는 것이 중요하다!
+
+// -----------------------------------------
+
+*/
