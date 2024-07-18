b3d9b3b381636ba4bfeb9ed890d41ac063138160
easyomi-code <easyomi.code@gmail.com>
2024-07-18 14:20:02 +0900

    challenge-2

---
+++ 01-Fundamentals-Part-1/Challenges/challenge-2.js
@@ -0,0 +1,44 @@
+// ----------------- # Challenge-2 # -------------------
+/*
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+// Challenge-1 코드 개선
+if(BMIMark > BMIJohn) {
+  console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`)
+} else {
+  console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
+}
+*/
+
+// ------------ # Challenge-2(solution) # --------------
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if(BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
