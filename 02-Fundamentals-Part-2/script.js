ef8fffdace053ecef97057e03a81e6242c060233
easyomi-code <easyomi.code@gmail.com>
2024-07-22 20:26:36 +0900

    Function

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,6 +1,7 @@
-// 1. Activating Strict Mode
+// 1. Activating Strict Mode (엄격 모드)
 'use strict';
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -9,3 +10,28 @@ if (hasDriversLicense) console.log('나는 운전을 할 수 있다 🚗');
 
 // const interface = 'Audio';
 // const private = 534;
+*/
+// -----------------------------------------
+
+// 2. Functions (함수)
+function logger() {
+  console.log('내 이름은 Eunyomi 입니다.');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  // console.log(apples, oranges);
+  const juice = `사과주스: ${apples}개, 오렌지주스: ${oranges}개`;
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 3);
+console.log(appleOrangeJuice);
+
+const num = Number('25');
