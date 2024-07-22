2a846e86a4b3e558f6579b0a27e14e16831c3b96
easyomi-code <easyomi.code@gmail.com>
2024-07-22 19:55:42 +0900

    Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,11 @@
+// 1. Activating Strict Mode
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('나는 운전을 할 수 있다 🚗');
+
+// const interface = 'Audio';
+// const private = 534;
