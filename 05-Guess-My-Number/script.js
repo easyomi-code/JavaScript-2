d8c154f6c406b90191b12107ff1b3e674a5001a5
easyomi-code <easyomi.code@gmail.com>
2024-08-06 13:22:13 +0900

    Handling Click Events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = '🎉 Correct Number!';
@@ -10,3 +11,14 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
