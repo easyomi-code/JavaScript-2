95acd0171830102f38c58ec0284959cf2687dd73
easyomi-code <easyomi.code@gmail.com>
2024-08-08 16:57:15 +0900

    Switching the Active Player

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -4,6 +4,8 @@
 // console.log(document.querySelector('#score--1').textContent);
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -14,11 +16,13 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+const scores = [0, 0];
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -35,9 +39,17 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score (현재 점수  )
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
     // 주사위 눈이 1일 경우
   } else {
     // switch to next player (Player 교체)
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    // toggle()
+    // : 특정 클래스가 요소에 이미 있으면 해당 클래스를 제거하고, 그렇지 않으면 클래스를 추가
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
