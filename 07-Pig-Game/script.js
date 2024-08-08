5728496cfc9c9bdf83616fea8d30a116b0da05ed
easyomi-code <easyomi.code@gmail.com>
2024-08-08 14:17:23 +0900

    PROJECT #3: Pig Game(코드 추가)

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+console.log(document.querySelector('#score--0').textContent);
+console.log(document.querySelector('#score--1').textContent);
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
