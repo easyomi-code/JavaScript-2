15eb0f33d0e1f6cca3c5e7da515ad8c43e997c1c
easyomi-code <easyomi.code@gmail.com>
2024-08-10 09:23:22 +0900

    Resetting the Game

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -16,15 +16,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Starting conditions
+// init(): 게임 초기화
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -89,3 +102,26 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+// 기능 3) New Game(Reset 버튼)
+// Challenge
+/*
+btnNew.addEventListener('click', function () {
+  playing = true;
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add('player--active');
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  scores[0] = 0;
+  scores[1] = 0;
+  activePlayer = 0;
+});
+*/
+
+// Solution
+btnNew.addEventListener('click', init);
