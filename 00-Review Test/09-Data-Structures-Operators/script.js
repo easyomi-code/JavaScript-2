98b7b538e27385a2bbc66aaf611cca1b14186eef
easyomi-code <easyomi.code@gmail.com>
2024-08-18 17:39:06 +0900

    Destructuring Arrays

--- 00-Review Test/09-Data-Structures-Operators/script.js
+++ 00-Review Test/09-Data-Structures-Operators/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// 1. Destructuring Arrays
+// 1.1
+const [firstBook, secondBook] = books;
+
+// 1.2
+const [, , thirdBook] = books;
+
+// 1.3
+const ratings = [
+  ['rating', 4.19],
+  ['ratingsCount', 144584],
+];
+const [[, rating], [, ratingsCount]] = ratings;
+
+// 1.4
+const ratingStars = [63405, 1808];
+const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
